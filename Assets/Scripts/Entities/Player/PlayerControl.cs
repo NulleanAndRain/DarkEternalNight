05e89fa35f9fb188b7311f6af303ba7f9beceb1d
Nullean <mr000lean@gmail.com>
2021-04-02 15:48:57 +0400

    created player idle animation

--- Assets/Scripts/Entities/Player/PlayerControl.cs
+++ Assets/Scripts/Entities/Player/PlayerControl.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof(WalkerComponent))]
 public class PlayerControl : MonoBehaviour {
     WalkerComponent walker;
+
+    bool isFacingLeft;
     void Start() {
         walker = GetComponent<WalkerComponent>();
     }
 
-    Vector3 _lsc = Vector3.one;
+    Vector3 _lsc;
     void Update() {
         float x = Input.GetAxis("Horizontal");
 
         if (x != 0) {
             walker.moveDir(x);
-            if (x < 0 && transform.localScale.x > 0 || x >0 && transform.localScale.x < 0) {
+            if (x < 0 && !isFacingLeft || x >0 && isFacingLeft) {
+                isFacingLeft = !isFacingLeft;
                 _lsc = transform.localScale;
                 _lsc.x *= -1;
                 transform.localScale = _lsc;
-			}
+            }
 		}
 
         if (Input.GetKeyDown(KeyCode.Space))
