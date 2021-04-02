27174ad6d9579b8541915e8014a16e475b092e52
Nullean <mr000lean@gmail.com>
2021-04-02 16:23:56 +0400

    fixed player sprite, added walk animation

--- Assets/Scripts/Entities/Player/PlayerControl.cs
+++ Assets/Scripts/Entities/Player/PlayerControl.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(WalkerComponent))]
+[RequireComponent(typeof(WalkerComponent), typeof(Animator))]
 public class PlayerControl : MonoBehaviour {
     WalkerComponent walker;
+    Animator anim;
 
     bool isFacingLeft;
     void Start() {
         walker = GetComponent<WalkerComponent>();
+        anim = GetComponent<Animator>();
     }
 
     Vector3 _lsc;
     void Update() {
         float x = Input.GetAxis("Horizontal");
 
-        if (x != 0) {
+        if (Mathf.Abs(x) >= 1e-4) {
             walker.moveDir(x);
             if (x < 0 && !isFacingLeft || x >0 && isFacingLeft) {
                 isFacingLeft = !isFacingLeft;
@@ -23,7 +25,11 @@ public class PlayerControl : MonoBehaviour {
                 _lsc.x *= -1;
                 transform.localScale = _lsc;
             }
-		}
+
+            anim.SetBool("isWalking", true);
+		} else {
+            anim.SetBool("isWalking", false);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
             walker.jump();
